f04b35de78329030f2c5cdfcea40de1f1e171a78
ningenMe <amohkuru@gmail.com>
2022-06-05 23:07:59 +0900

    [fix]

--- zeus/domain/build.gradle.kts
+++ zeus/domain/build.gradle.kts
@@ -3,9 +3,6 @@ tasks {
         enabled = false
     }
 }
-repositories {
-    maven { url = uri("https://jitpack.io") }
-}
 dependencies {
     implementation("com.github.guepardoapps:kulid:2.0.0.0")
 }
