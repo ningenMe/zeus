f6d39a815adbd6b5f12346d12da71fefeca389c3
ningenMe <amohkuru@gmail.com>
2022-06-19 23:56:52 +0900

    [add] run config

--- zeus/api/src/main/kotlin/ningenme/net/zeus/ApiApplication.kt
+++ zeus/api/src/main/kotlin/ningenme/net/zeus/ApiApplication.kt
@@ -1,11 +1,12 @@
 package ningenme.net.zeus
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 open class ApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<ApiApplication>(*args)
+    runApplication<ApiApplication>(*args)
 }
