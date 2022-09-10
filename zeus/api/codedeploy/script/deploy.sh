e9e565029d22a7b2d5dd65bc0828cdc797c4892f
ningenMe <amohkuru@gmail.com>
2022-09-10 21:43:33 +0900

    [fix]

--- zeus/api/codedeploy/script/deploy.sh
+++ zeus/api/codedeploy/script/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 aws ecr get-login-password --region AWS_REGION | docker login --username AWS --password-stdin AWS_ECR_REGISTRY
+aws ssm get-parameters-by-path --path "/" --region ap-northeast-1 --output text | awk '{print $5"="$7}' > /home/ec2-user/.env
+
 docker pull AWS_ECR_REGISTRY/AWS_ECR_REPOSITORY:latest
 docker container stop zeus-api | true
 docker container rm zeus-api | true
-docker run -d -p8080:8080 --name zeus-api AWS_ECR_REGISTRY/AWS_ECR_REPOSITORY:latest
+docker run -d -p8080:8080 --name zeus-api --env-file "/home/ec2-user/.env" AWS_ECR_REGISTRY/AWS_ECR_REPOSITORY:latest
 docker rmi `docker image ls | grep none | awk '{print $3}'` | true
